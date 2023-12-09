3b6b80f253f56628db62b961a861c5caa747a9a8
MarznaHuzei <marinka777xx@gmail.com>
2023-12-09 15:22:11 +0100

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,59 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.floor(Math.random());
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome");
+    } else {
+      console.log("This key doesn't fit");
+    }
+  }
+  OpenDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Welcome");
+    } else {
+      console.log("This key doesn't fit");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
